25ec6e0c0356172b7abe7b3e82a6475f89c5744b
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-01 08:38:02 -0400

    Comments and explanation added.

--- CPP-PRG-09-03-Drop-Lowest-Score/main.cpp
+++ CPP-PRG-09-03-Drop-Lowest-Score/main.cpp
@@ -13,75 +13,102 @@
 
 using namespace std;
 
-void getNumberOfTestScores(int &);
-double *getTestScores(int);
-double *sortScores(double *, int);
+// Function prototypes
+int *getNumberOfTestScores();
+double *getTestScores(int *);
+double *sortScores(double *, int *);
 void swap(double *, double *);
-double averageTestScore(double *, int);
-void showOutputMessage(double *, int, double);
+double *averageTestScore(double *, int *);
+void showOutputMessage(double *, int *, double *);
 
 
 int main()
 {
-    int intNumScores;
+    // Create integer pointer and set to null for number
+    // of test scores for use with getNumberOfTestScores()
+    int *intNumScores = nullptr;
     
+    // Create two double pointers to hold the array of
+    // test scores and the average test score
     double *dblTestScores = nullptr;
-    double dblAverageScore;
+    double *dblAverageScore = nullptr;
     
-    getNumberOfTestScores(intNumScores);
+    // Get value for intNumScores
+    intNumScores = getNumberOfTestScores();
     
+    // Get values for test scores array
     dblTestScores = getTestScores(intNumScores);
     
-    //    Debug
-    //    for (int i = 0 ; i < intNumScores ; i++)
-    //    {
-    //        cout << *(dblTestScores + i) << endl;
-    //    }
-    
+    // For spacing between test score entry and displaying
+    // the sorted array and average score.
     cout << endl << endl;
     
+    // Sort the testScores array in descending order
     dblTestScores = sortScores(dblTestScores, intNumScores);
     
-    //    Debug
-    //    for (int i = 0 ; i < intNumScores ; i++)
-    //    {
-    //        cout << *(dblTestScores + i) << endl;
-    //    }
-    
+    // Calculate the average test score (order of sortScores and
+    // averageTestScore can be swapped if necessary, arbitrary
+    // decision to sort before calculating average)
     dblAverageScore = averageTestScore(dblTestScores, intNumScores);
     
+    // Pass pointers through to display them in a formatted
+    // output message(s)
     showOutputMessage(dblTestScores, intNumScores, dblAverageScore);
     
+    // Exit
     return 0;
 }
 
-void getNumberOfTestScores(int &refNumScores)
+// This function gets an integer value from the user and performs
+// rudimentary data validation on it until the user enters a number
+// between 0 and 10 for the total number of test scores to sort
+// and average.
+int *getNumberOfTestScores()
 {
-    //    int intNumScores;
+    // Declare new pointer, set to null
+    int *intNumScores = nullptr;
     
+    // Set null pointer as a new int variable
+    intNumScores = new int;
+    
+    // Get user input as pointer value and performs simple data validation
     cout << "How many test scores would you like to average?\n";
-    cin >> refNumScores;
-    while(!cin || refNumScores <= 0 || refNumScores > 10)
+    cin >> *intNumScores;
+    // Data validation
+    while(!cin || *intNumScores <= 0 || *intNumScores > 10)
     {
         cout << "Please enter a number between 1 and 10.\n";
         cin.clear();
         cin.ignore();
-        cin >> refNumScores;
+        cin >> *intNumScores;
     }
+    
+    // Return the address of the number of tests the
+    // array will hold
+    return intNumScores;
 }
 
-double *getTestScores(int intScores)
+// This function takes the user generated intNumScores from
+// the last function and uses it to dynamically generate
+// an array of that size and also gets user inputs (and
+// performs data validation) to fill the array.
+double *getTestScores(int *intScores)
 {
+    // Create new pointer, set to null
     double *dblArr = nullptr;
     
-    dblArr = new double[intScores];
+    // Describe null pointer as a new array with size intScores
+    dblArr = new double[*intScores];
     
-    //    unique_ptr<double[]> dblArr (new double[intScores]);
-    
-    for (int i = 0 ; i < intScores ; i++)
+    // Loop through array one time and fill each element
+    // with user input of tests scores
+    for (int i = 0 ; i < *intScores ; i++)
     {
+        // Show prompt displaying which element the user is
+        // entering out of the total number of elements
         cout << "Please enter your #" << i + 1 << " test score.\n";
         cin >> dblArr[i];
+        // Data validation
         while(!dblArr[i] || dblArr[i] < 0 || dblArr[i] > 100)
         {
             cout << "Please enter a test score between 0 and 100.\n";
@@ -91,6 +118,7 @@ double *getTestScores(int intScores)
         }
     }
     
+    // Return the array address to main
     return dblArr;
     
     //    HOW TO MAKE THIS WORK?!
@@ -98,64 +126,110 @@ double *getTestScores(int intScores)
     //    dblArr = nullptr;
 }
 
-double *sortScores(double *dblScores, int INT_SCORES)
+// This function takes the values of the array of test scores
+// and performs a selection sort on them
+double *sortScores(double *dblScores, int *INT_SCORES)
 {
+    // Used to hold the smallest value (index of starting element
+    // for each pass through the array)
     int intMinIndex;
+    // Used to keep track of the beginning element of each sweep
+    // through the array
     int intStartScan;
     
+    // Declare pointer variable and set to null to hold array
+    // value when swapping
     double *dblMinElement = nullptr;
     
-    for(intStartScan = 0 ; intStartScan < (INT_SCORES - 1) ; intStartScan++)
+    // Loop through the array one element at a time
+    for(intStartScan = 0 ; intStartScan < (*INT_SCORES - 1) ; intStartScan++)
     {
+        // Set min index (first element) in subscan to startscan
         intMinIndex = intStartScan;
+        // Set the pointer equal to the memory location of that
+        // array element
         dblMinElement = &dblScores[intStartScan];
         
-        for(int index = intStartScan + 1 ; index < INT_SCORES ; index++)
+        // Loop through all remaining array elements...
+        for(int index = intStartScan + 1 ; index < *INT_SCORES ; index++)
         {
+            // ...and look for the value of a smaller element...
             if(dblScores[index] < *dblMinElement)
             {
+                // ...if found, set the holder pointer variable address
+                // to the smallest found value
                 dblMinElement = &dblScores[index];
+                // and set the minimum value index to that array element
                 intMinIndex = index;
             }
         }
+        // And swap the beginning of the subloop through's element
+        // with the found smallest element
         swap(dblScores[intMinIndex], dblScores[intStartScan]);
     }
     
+    // Return the address of the sorted array
     return dblScores;
 }
 
+// Simple swap function
 void swap(double *dblA, double *dblB)
 {
+    // Use pointers, create temp variable to
+    // hold address of values to be swapped
     double *temp = nullptr;
     
+    // Perform swap using addresses instead
+    // of variables
     temp = dblA;
     dblA = dblB;
     dblB = temp;
 }
 
-double averageTestScore(double *dblScores, int INT_SCORES)
+// This function calculates the value of the average test score
+double *averageTestScore(double *dblScores, int *INT_SCORES)
 {
-    double dblTotal = 0;
-    double dblAverage;
+    // Create new pointer variables to hold the total score (for
+    // calculating the average score) and the calculated average score
+    double *dblTotal = nullptr;
+    double *dblAverage = nullptr;
     
-    for(int i = 1 ; i < INT_SCORES ; i++)
+    // Define null pointers as new double variables
+    dblTotal = new double;
+    dblAverage = new double;
+    
+    // Set running total dblTotal counter to 0 to start
+    *dblTotal = 0;
+    
+    // Because the array is sorted, we just skip the first
+    // element [0] and start the counter at 1 instead of 0...
+    for(int i = 1 ; i < *INT_SCORES ; i++)
     {
-        dblTotal += *(dblScores + i);
+        *dblTotal += *(dblScores + i);
     }
     
-    dblAverage = dblTotal / (INT_SCORES - 1);
+    // ...then divide that total by the total number of scores less one
+    *dblAverage = *dblTotal / (*INT_SCORES - 1);
     
+    // Return the address of the calculated average score value
     return dblAverage;
 }
 
-void showOutputMessage(double *dblScores, int INT_SCORES, double dblAverage)
+// This function displays the calculated results and sorted array
+// in a formatted fashion.
+void showOutputMessage(double *dblScores, int *INT_SCORES, double *dblAverage)
 {
+    // Set precisions so that scores and average value only
+    // have one decimal point visible
     cout << setprecision(1) << fixed << showpoint;
     cout << "Your test scores sorted in order are:\n";
-    for(int i = 0 ; i < INT_SCORES ; i++)
+    // Loop through array and output contents to console
+    for(int i = 0 ; i < *INT_SCORES ; i++)
     {
         cout << *(dblScores + i) << endl;
     }
+    // Output message declaring which score was dropped
     cout << "Your score of " << *(dblScores) << " was dropped.\n";
-    cout << "Your new average score was " << dblAverage << endl;
+    // Output message and calculated average value to console
+    cout << "Your new average score was " << *dblAverage << endl;
 }
